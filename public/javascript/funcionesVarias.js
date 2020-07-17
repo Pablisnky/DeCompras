571d2b0547d48525a6b834709de0bc853a8b85c1
PabloCabeza <pcabeza7@gmail.com>
2020-07-16 20:35:55 -0400

    Se realizaron varias funciones para la logica del frontEnd, aún no son definitivas, la aplicación se esta manejando en un solo departamento y una sola sección para luego extrapolar a todos los elementos

--- public/javascript/funcionesVarias.js
+++ public/javascript/funcionesVarias.js
@@ -11,3 +11,128 @@ function ocultarDiv(id){
         document.getElementById(id).style.display = "none";
     }
 }
+
+//************************************************************************************************
+//Llamada desde vitrina_V
+    function verOpciones(contador, contador_2){      
+        //Se captura el valor de los id dinanmicos que se genern y se guarda en ID_Dinamico
+        window.localStorage.setItem('ID_Dinamico', contador);
+        window.localStorage.setItem('ID_contenedorSombreado', contador_2);
+        let H = localStorage.getItem("ID_Dinamico");
+        let I = localStorage.getItem("ID_contenedorSombreado");
+        console.log("ID_Dinamico= " + H);  
+        console.log("ID_contenedorSombreado= " + I);  
+                
+        document.getElementById("Mostrar_Opciones").style.display = "block";
+        document.getElementsByTagName("html")[0].style.overflow = "hidden";
+        document.getElementById(I).style.backgroundColor = "rgba(165, 42, 42, 0.3)";
+    }
+
+//************************************************************************************************
+//Llamada desde vitrina_V.php
+        function CerrarModal_X(){
+            document.getElementById("Mostrar_Opciones").style.display = "none";
+            document.getElementsByTagName("html")[0].style.overflow = "visible";
+        }
+
+//************************************************************************************************
+//Llamada desde funcionesVarias.js 
+    function CerrarModal(){
+        let Z = document.getElementsByClassName("contenedor_14").id = localStorage.getItem('ID_Dinamico'); 
+        console.log("El ID del contenedor: " + Z);
+        document.getElementById(Z).style.display = "grid";
+        document.getElementById("Mostrar_Opciones").style.display = "none";
+        document.getElementsByTagName("html")[0].style.overflow = "visible";
+    }
+
+//************************************************************************************************
+//Envia el valor de la opcion del producto seleccionado aL html que llamada desde vitrina_V
+    function transferirOpcion(ID_inputTalla){
+        //Se captura el valor del elemento con id dinanmico que contiene el tamaño de la prenda 
+        window.localStorage.setItem('ID_InputTalla', ID_inputTalla);
+        let F = document.getElementsByClassName("label_l").id = localStorage.getItem('ID_InputTalla');
+        console.log("ID_InputT= " + F);  
+
+        let Opcion = document.getElementsByName("talla");
+        for(var i=0; i<Opcion.length; i++){
+            if(Opcion[i].checked){
+                Opcion= Opcion[i].value;
+                //Despues de recorrer las opciones se guarda la opcion checkeada en un localStorage
+                window.localStorage.setItem('tamanioPrenda', Opcion);
+                console.log("La talla de la prenda es: " + localStorage.getItem("tamanioPrenda"));
+                console.log("El ID del input talla= " + F);
+    
+                // Se cambia el valor de la talla            
+                // document.getElementById(F).value = localStorage.getItem("tamanioPrenda");   
+            }
+        }
+        
+        pedidoParcial()
+        CerrarModal();
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//************************************************************************************************
+//Llamada desde vitrina_V
+function incrementar(){
+    valor = document.getElementById("Item");
+    if(valor.value < 10){
+        A = valor.value ++;
+        A++;
+        document.getElementById("Cantidad").value = A;
+        console.log(A);
+    }
+    pedidoParcial();
+}
+
+//************************************************************************************************
+//Llamada desde vitrina_V
+function decrementar(){
+    valor = document.getElementById("Item");
+    if(valor.value > 01){
+        B = valor.value --;
+        B--;
+        document.getElementById("Cantidad").value = B;
+        console.log(B);
+    }
+
+    pedidoParcial();
+} 
+
+//************************************************************************************************
+//Llamada desde vitrina_V
+function pedidoParcial(){
+    
+    let Cantidad = document.getElementById("Cantidad").value;
+    let Precio = document.getElementById("Precio").value;
+
+    let Total = Cantidad * Precio;    
+    document.getElementById("Total").value = Total;
+
+    document.getElementById("Leyenda").value = Cantidad + " x " + localStorage.getItem("tamanioPrenda") + " = " + Total;    
+    console.log(Total);
+}
