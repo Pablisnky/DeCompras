904dffcd32457d6092ce11aea13aad364fc2d842
PabloCabeza <pcabeza7@gmail.com>
2020-07-22 05:16:17 -0400

    - Los campos ocultos desde el HTML con "hidden" se oculatan ahora con una clase CSS. -

--- app/vistas/paginas/vitrina_V.php
+++ app/vistas/paginas/vitrina_V.php
@@ -16,7 +16,7 @@
                     <div class='contenedor_11' onclick="verOpciones('<?php echo 'Cont_Sombreado_' .$Contador;?>','<?php echo 'Cont_Dinamico_' . $Contador;?>','<?php echo 'Talla_' . $Contador;?>','<?php echo 3 . $Contador;?>','<?php echo 'Leyenda_'. $Contador;?>','<?php echo 'Precio_' . $Contador;?>','<?php echo 'Cantidad_' . $Contador;?>','<?php echo 'Item_'. $Contador;?>'); llamar_Opciones('<?php echo $ID_Departamento;?>','<?php echo $Nombre;?>', 'No','No','No')">
                         <div class="contenedor_12">
                             <figure>
-                                <img class="image_1" src="https://placeimg.com/250/250/animals" alt="Imagen"/>
+                                <img class="image_1" src="" alt="Imagen"/>
                             </figure>
                         </div>
                         <div>
@@ -24,7 +24,7 @@
                         </div>
                         <div style="text-align:right;">
                             <label class="label_2">$ <?php echo $Precio;?></label>
-                            <input id="<?php echo 'Precio_' . $Contador;?>" value="<?php echo $Precio;?>" hidden/>                           
+                            <input class="input_3" id="<?php echo 'Precio_' . $Contador;?>" value="<?php echo $Precio;?>"/>                           
                         </div>
                         <?php 
                         if(!empty($UnidadVenta)){ ?>
@@ -34,25 +34,23 @@
                         }   ?>                    
                     </div>
                 </article>
-                <article><!--Detalles del producto selecionado -->
+                <article><!--Detalles del producto selecionado, este es el DIV clonado -->
                     <div class="contenedor_14" id="<?php echo 'Cont_Sombreado_' . $Contador;?>">
                         <div class="contenedor_20" id="<?php echo 'Cont_Agrega_Opcion_' . $Contador;?>">
                             <div class="contenedor_19" id="<?php echo 'Cont_A_Clonar_' . $Contador;?>">
                                 <div>
                                     <!-- cantidad -->
-                                    <input type="text" class="label_1e" id="<?php echo 'Cantidad_' . $Contador;?>" value="1" hidden>
+                                    <input type="text" class="label_1e input_3" id="<?php echo 'Cantidad_' . $Contador;?>" value="1"/>
                                     <!-- talla -->
-                                    <input type="text" class="label_1a" id="<?php echo 'Talla_' . $Contador;?>" value="<?php echo $Opcion ?>" hidden>
+                                    <input type="text" class="label_1a input_3" id="<?php echo 'Talla_' . $Contador;?>" value="<?php echo $Opcion ?>"/>
                                     <!-- Precio -->
-                                    <input type="text" class="label_1d" id="<?php echo 3 . $Contador;?>" hidden>
+                                    <input type="text" class="label_1d input_3" id="<?php echo 3 . $Contador;?>"/>
                                     <!-- Leyenda -->
-                                    <input type="text" class="label_1c" id="<?php echo 'Leyenda_'. $Contador;?>">  
+                                    <input type="text" class="label_1c" id="<?php echo 'Leyenda_'. $Contador;?>"/>
                                 </div> 
                                 <div class="contenedor_16">
-                                    <button class="button_1" onclick="decrementar();">-</button>
-                                    
-                                    <input class="input_2 input2a" type="text" id="<?php echo 'Item_'. $Contador;?>"  value="1" disabled>
-
+                                    <button class="button_1" id="boton" onclick="decrementar();">-</button>
+                                    <input class="input_2 input2a" type="text" id="<?php echo 'Item_'. $Contador;?>"  value="1" disabled="disabled"/>
                                     <button class="button_2" id="<?php echo 'BotonInc_' .  $Contador;?>" onclick="incrementar()">+</button>
                                 </div> 
                             </div>
@@ -62,7 +60,7 @@
                                 <label class="label_5" onclick="llamar_Opciones('<?php echo $ID_Departamento;?>','<?php echo $Nombre;?>','Si','<?php echo 'Cont_Agrega_Opcion_' . $Contador;?>','<?php echo 'Cont_A_Clonar_' . $Contador;?>')">Agregar otra opción</label> 
                             </div> 
                             <div>
-                                <input class="input_4" type="text" name="AgregarOpc" placeholder="Agregar aclaración">
+                                <input class="input_4" type="text" name="AgregarOpc" placeholder="Agregar aclaración"/>
                             </div> 
                         </div>  
                     </div>
@@ -71,18 +69,12 @@
             </section>
             <?php
             $Contador++;
-        }   
+        }
         ?>
     </div>
 </section>
 
-<!-- //Trae por medio de Ajax las opciones de cada pedido, los valores los toma desde opciones_V.php -->
-<div id="Mostrar_Opciones"></div>
-
-
-
-
-
-
-
-
+<section>
+<!--Trae por medio de Ajax las opciones de cada pedido, los valores los toma desde opciones_V.php -->
+    <div id="Mostrar_Opciones"></div>
+</section>
