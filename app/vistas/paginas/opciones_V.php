dda7dcc47ed98610eb7c880f1a3fae694a289ac6
PabloCabeza <pcabeza7@gmail.com>
2020-07-22 05:13:21 -0400

    - Se organizó el marcado HTML y se realizaron comentarios Please enter the commit message for your changes. Lines starting

--- app/vistas/paginas/opciones_V.php
+++ app/vistas/paginas/opciones_V.php
@@ -1,36 +1,35 @@
 <!-- Archivo cargado por petición Ajax desde funcionesAjax.js en la función llamar_Opciones() -->
-<section class="section_3" >
+<section class="section_3">
     <form>
         <div class="contenedor_13">
-            <span class="span_5" onclick="CerrarModal_X()">X</span>
+            <span class="span_5" id="Span_5" onclick="CerrarModal_X()">X</span>
             <label class="label_3">Elija una opción</label>         
-                <?php   
-                if($Datos["AgregarNodo"] == 'No'){
-                    $ContadorLabel = 1;
-                    foreach($Datos['Inf_Consulta'] as $row){
-                        $Opcion = $row['opciones'];
-                        ?>                      
-                        <input id="<?php echo 'ContadorLabel_' . $ContadorLabel?>" type="radio" name="opcion" value="<?php echo $Opcion?>" onclick="transferirOpcion(this.form)" hidden>
-                        <label for="<?php echo 'ContadorLabel_' . $ContadorLabel?>" class="label_4"><?php echo $Opcion?></label> 
-                            <?php 
+            <?php   
+            if($Datos["AgregarNodo"] == 'No'){
+                $ContadorLabel = 1;
+                foreach($Datos['Inf_Consulta'] as $row){
+                    $Opcion = $row['opciones'];
+                    ?>                      
+                    <input id="<?php echo 'ContadorLabel_' . $ContadorLabel;?>" type="radio" name="opcion" value="<?php echo $Opcion;?>" onclick="transferirOpcion(this.form)" hidden/>
+                    <label for="<?php echo 'ContadorLabel_' . $ContadorLabel;?>" class="label_4"><?php echo $Opcion;?></label> 
+                    <?php 
                     $ContadorLabel++;
-                    }
                 }
-                else{
-                    $ContadorLabel = 1;
-                    $ContPadre = $Datos["Cont_Pad"];
-                    $ContAClonar = $Datos["Cont_a_Clonar"];
-                    foreach($Datos['Inf_Consulta'] as $row){
-                        $Opcion = $row['opciones'];
-                        ?>                      
-                        <input id="<?php echo 'ContadorLabel_' . $ContadorLabel?>" type="radio" name="opcion" value="<?php echo $Opcion?>" onclick="AgregaOpcion(this.form, '<?php echo $ContPadre?>','<?php echo $ContAClonar;?>')" hidden>
-                        <label for="<?php echo 'ContadorLabel_' . $ContadorLabel?>" class="label_4"><?php echo $Opcion?></label> 
-                            <?php 
-                    $ContadorLabel++;
-                    }
-                }   
+            }
+            else{
+                $ContadorLabel = 1;
+                $ContPadre = $Datos["Cont_Pad"]; //Trae el ID del contenedor padre
+                $ContAClonar = $Datos["Cont_a_Clonar"];//Trae elID del contenedor a Clonar
+                foreach($Datos['Inf_Consulta'] as $row){
+                    $Opcion = $row['opciones'];
+                    ?>                      
+                    <input id="<?php echo 'ContadorLabel_' . $ContadorLabel?>" type="radio" name="opcion" value="<?php echo $Opcion?>" onclick="AgregaOpcion(this.form, '<?php echo $ContPadre?>','<?php echo $ContAClonar;?>')" hidden>
+                    <label for="<?php echo 'ContadorLabel_' . $ContadorLabel?>" class="label_4"><?php echo $Opcion?></label> 
+                    <?php 
+                $ContadorLabel++;
+                }
+            }   
             ?>                    
         </div>
     </form>
-</section> 
-  
+</section>   
