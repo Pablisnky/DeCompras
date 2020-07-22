07e1e8ff5abff2cb340cffd609eaad6943f9caa2
PabloCabeza <pcabeza7@gmail.com>
2020-07-22 05:10:59 -0400

    - Todos los llamados a eventos javascript se cambiaron a la forma addEventListener.

--- app/vistas/paginas/inicio_V.php
+++ app/vistas/paginas/inicio_V.php
@@ -5,11 +5,11 @@
             <div class='contenedor_6'>
                 <div class="contenedor_9">
                     <div>
-                        <span class="icon-newspaper span_3" onclick="Llamar_Delivery()"></span>
-                        <h2 class='h2_1' onclick="Llamar_Delivery(); ocultarDiv('Mostrar_Delivery')">DELIVERY Y ROTISERIA</h2>
+                        <span class="icon-newspaper span_3"></span>
+                        <h2 class='h2_1' id="H2_1">DELIVERY Y ROTISERIA</h2>
                     </div>
                     <div>
-                        <span class="icon-arrow-down2 span_4" onclick="Llamar_Delivery()"></span>
+                        <span class="icon-arrow-down2 span_4"></span>
                     </div>
                 </div>
                 <div class="contenedor_5" id="Mostrar_Delivery"></div> 
@@ -18,11 +18,11 @@
             <div class='contenedor_6'>
                 <div class="contenedor_9">
                     <div>
-                        <span class="icon-newspaper span_3" onclick="Llamar_Ropa()"></span>
-                        <h2 class='h2_1' onclick="Llamar_Ropa(); ocultarDiv('Mostrar_Ropa')">ROPA E INDUMENTARIA</h2>
+                        <span class="icon-newspaper span_3"></span>
+                        <h2 class='h2_1' id="H2_2">ROPA E INDUMENTARIA</h2>
                     </div>
                     <div>
-                        <span class="icon-arrow-down2 span_4" onclick="Llamar_Ropa()"></span>
+                        <span class="icon-arrow-down2 span_4"></span>
                     </div>
                 </div>
                 <div class="contenedor_5" id="Mostrar_Ropa"></div> 
@@ -31,11 +31,11 @@
             <div class='contenedor_6'>
                 <div class="contenedor_9">
                     <div>
-                        <span class="icon-newspaper span_3" onclick="Llamar_Bar()"></span>
-                        <h2 class='h2_1' onclick="Llamar_Bar(); ocultarDiv('Mostrar_Bar')">BAR Y RESTO</h2>
+                        <span class="icon-newspaper span_3"></span>
+                        <h2 class='h2_1' id="H2_3">BAR Y RESTO</h2>
                     </div>
                     <div>
-                        <span class="icon-arrow-down2 span_4" onclick="Llamar_Bar()"></span>
+                        <span class="icon-arrow-down2 span_4"></span>
                     </div>
                 </div>
                 <div class="contenedor_5" id="Mostrar_Bar"></div> 
@@ -44,11 +44,11 @@
             <div class='contenedor_6'>
                 <div class="contenedor_9">
                     <div>
-                        <span class="icon-newspaper span_3" onclick="Llamar_Alimentos()"></span>
-                        <h2 class='h2_1' onclick="Llamar_Alimentos(); ocultarDiv('Mostrar_Alimentos')">ALIMENTOS FRESCOS</h2>
+                        <span class="icon-newspaper span_3"></span>
+                        <h2 class='h2_1' id="H2_4">ALIMENTOS FRESCOS</h2>
                     </div>
                     <div>
-                        <span class="icon-arrow-down2 span_4" onclick="Llamar_Alimentos()"></span>
+                        <span class="icon-arrow-down2 span_4"></span>
                     </div>
                 </div>  
                 <div class="contenedor_5" id="Mostrar_Alimentos"></div> 
@@ -57,11 +57,11 @@
             <div class='contenedor_6'>
                 <div class="contenedor_9">
                     <div>
-                        <span class="icon-newspaper span_3" onclick="Llamar_Productos()"></span>
-                        <h2 class='h2_1' onclick="Llamar_Productos(); ocultarDiv('Mostrar_Productos')">PRODUCTOS Y RETAIL</h2>
+                        <span class="icon-newspaper span_3"></span>
+                        <h2 class='h2_1' id="H2_5">PRODUCTOS Y RETAIL</h2>
                     </div>
                     <div>
-                        <span class="icon-arrow-down2 span_4" onclick="Llamar_Productos()"></span>
+                        <span class="icon-arrow-down2 span_4"></span>
                     </div>
                 </div>
                 <div class="contenedor_5" id="Mostrar_Productos"></div> 
@@ -70,11 +70,11 @@
             <div class='contenedor_6'>
                 <div class="contenedor_9">
                     <div>
-                        <span class="icon-newspaper span_3" onclick="Llamar_Maquinas()"></span>
-                        <h2 class='h2_1' onclick="Llamar_Maquinas(); ocultarDiv('Mostrar_Maquinas')">MAQUINAS POR TIEMPO</h2>
+                        <span class="icon-newspaper span_3"></span>
+                        <h2 class='h2_1' id="H2_6">MAQUINAS POR TIEMPO</h2>
                     </div>
                     <div>
-                        <span class="icon-arrow-down2 span_4" onclick="Llamar_Maquinas()"></span>
+                        <span class="icon-arrow-down2 span_4"></span>
                     </div>
                 </div>
                 <div class="contenedor_5" id="Mostrar_Maquinas"></div> 
