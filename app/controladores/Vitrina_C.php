29f1dd6b4b6323e82d8633ab52dc25ab9a608f0c
PabloCabeza <pcabeza7@gmail.com>
2020-07-25 07:00:57 -0400

    - Se amplio la funcionabilidad del metodo index, ahora todas las consultas de productos por tiendas se hacen desde una unica consulta  Please enter the commit message for your changes. Lines starting

--- app/controladores/Vitrina_C.php
+++ app/controladores/Vitrina_C.php
@@ -5,25 +5,36 @@
             $this->ConsultaVitrina_M = $this->modelo("Vitrina_M");
         }
         
-        public function index(){
-            $this->vista("paginas/vitrina_V");
-        }
-        
-        public function Delivery(){
-            //Se CONSULTAN el departamento de Delivery y Rotiseria
-            $Consulta = $this->ConsultaVitrina_M->consultarDelivery();
-            $Datos = $Consulta->fetchAll(PDO::FETCH_BOTH);
-            
+        //Se CONSULTAN los productos de la tienda seleccionada, llamada desde funcionesVarios.js en la funcion vitrina()
+        public function index($ID_Tienda){
+            $Consulta_1 = $this->ConsultaVitrina_M->consultarProductos($ID_Tienda);
+            $Productos = $Consulta_1->fetchAll(PDO::FETCH_BOTH);  
+
+            $Consulta_2 = $this->ConsultaVitrina_M->consultarNombreTienda($ID_Tienda);
+            $Nombre = $Consulta_2->fetch(PDO::FETCH_ASSOC);
+            $Datos=[
+                "Inf_Productos" => $Productos,
+                "NombreTienda" => $Nombre,
+            ];
+
             $this->vista("paginas/vitrina_V",  $Datos);
         }
+        
+        // public function Delivery(){
+        //     //Se CONSULTAN el departamento de Delivery y Rotiseria
+        //     $Consulta = $this->ConsultaVitrina_M->consultarDelivery();
+        //     $Datos = $Consulta->fetchAll(PDO::FETCH_BOTH);
+            
+        //     $this->vista("paginas/vitrina_V",  $Datos);
+        // }
 
-        public function Ropa(){
+        //public function Ropa(){
             //Se CONSULTAN los Licores y comidas de restaurant
-            $Consulta = $this->ConsultaVitrina_M->consultarRopa();
-            $Datos = $Consulta->fetchAll(PDO::FETCH_BOTH);
+            // $Consulta = $this->ConsultaVitrina_M->consultarRopa();
+            // $Datos = $Consulta->fetchAll(PDO::FETCH_BOTH);
 
-            $this->vista("paginas/vitrina_V", $Datos);
-        }
+            // $this->vista("paginas/vitrina_V", $Datos);
+        //}
 
         // public function Bar(){
         //     //Se CONSULTAN los Licores y comidas de restaurant
