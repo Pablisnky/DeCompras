2ea312dd587486f2dbca04acc43eb5b482e92a0d
PabloCabeza <pcabeza7@gmail.com>
2020-07-25 07:03:52 -0400

    - Los atributos hidden en el HTML se eliminaron y se coloran por medio de una clase CSS  Please enter the commit message for your changes. Lines starting

--- app/modelos/Opciones_M.php
+++ app/modelos/Opciones_M.php
@@ -7,8 +7,8 @@
             parent::__construct();  
         }
         
-        public function consultarOpciones($ID_Departamento, $NombreProducto){
-            $stmt = $this->dbh->prepare("SELECT * FROM opciones INNER JOIN secciones ON opciones.ID_Seccion=secciones.ID_Seccion WHERE opciones.ID_Departamento = '$ID_Departamento' AND seccion = '$NombreProducto'");      
+        public function consultarOpciones($ID_Tienda, $Producto){
+            $stmt = $this->dbh->prepare("SELECT * FROM opciones INNER JOIN productos_opciones ON opciones.ID_Opcion=productos_opciones.ID_Opcion INNER JOIN tiendas_productos ON tiendas_productos.ID_Producto=productos_opciones.ID_Producto INNER JOIN productos ON tiendas_productos.ID_Producto=productos.ID_Producto WHERE tiendas_productos.ID_Tienda = '$ID_Tienda' AND productos.producto = '$Producto'");      
             if($stmt->execute()){
                 return $stmt;
             }
@@ -18,7 +18,7 @@
         }
 
         // public function consultarOpcionesDelivery($Departamento, $ID_Producto){
-        //     $stmt = $this->dbh->prepare("SELECT * FROM delivery INNER JOIN descripcion_delivery ON delivery.ID_Producto=descripcion_delivery.ID_Producto WHERE ID_Departamento = $Departamento AND delivery.ID_Producto = $ID_Producto");      
+        //     $stmt = $this->dbh->prepare("SELECT * FROM delivery INNER JOIN descripcion_delivery ON delivery.ID_Producto=descripcion_delivery.ID_Producto WHERE ID_Tienda = $Departamento AND delivery.ID_Producto = $ID_Producto");      
         //     if($stmt->execute()){
         //         return $stmt;
         //     }
