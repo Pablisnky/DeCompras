97b70ea333cba97e5e162ce0eef2a3ec42275b40
PabloCabeza <pcabeza7@gmail.com>
2021-01-07 05:21:52 -0400

    Se añadio la ventana modal fuera de horario  Please enter the commit message for your changes. Lines starting

---
+++ app/vistas/modal/modal_fueraHorario_V.php
@@ -0,0 +1,13 @@
+<?php require(RUTA_APP . "/vistas/inc/header.php");  ?>
+
+<section class="section_4 section_10"  id="Ejemplo_Secciones">
+    <div class="contenedor_24 contenedor_118">
+        <h1 class="h1_1">Regresamos a las 02:00 pm.</h1>
+        <p>Puedes realizar tu compra, la operación de despacho iniciará cuando regresemos</p>
+    </div>
+    <div class="contenedor_87">                
+        <a class="label_21 boton" href="<?php echo RUTA_URL . '/Cuenta_C/Publicar/';?>">Cerrar</a>
+    </div>
+    </div>
+</section>
+
